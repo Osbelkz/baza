65f27c8d667292818824b3633ceea2b38232062d
Askar Beldenov <askarbeldenov@gmail.com>
2022-01-22 14:16:38 +0600

    fix

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_AUTH_URL = "http://localhost:8080/";
+export const API_AUTH_URL = "https://baza-application.herokuapp.com/";
 
 export const apiInstance = axios.create({
   baseURL: API_AUTH_URL,
